8d30909e5ec4205bd1d590ca5176829ee038754a
joeyst <joeystenbeck@gmail.com>
2023-12-07 22:36:18 -0800

    Add LAYERS_PER_BLOCK, NEURONS_PER_LAYER, LEARNING_RATE.

--- config.h
+++ config.h
@@ -3,6 +3,9 @@
 #define _CONFIG_H 
 
 const float NEURON_FIRE_THRESH = 1.f;
-const float FIRING_RATE_GOAL = 4.f; // Per 32 ticks. 
+const float FIRING_RATE_GOAL   = 4.f; // Per 32 ticks. 
+const int   LAYERS_PER_BLOCK   = 4;
+const int   NEURONS_PER_LAYER  = 4;
+const float LEARNING_RATE      = 0.01f;
 
 #endif 
