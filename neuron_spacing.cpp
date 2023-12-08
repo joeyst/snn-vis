e9bb80b11fced1cdd9c6d903f193166f2ba7c668
joeyst <joeystenbeck@gmail.com>
2023-12-08 03:53:13 -0800

    WIP.

--- neuron_spacing.cpp
+++ neuron_spacing.cpp
@@ -4,3 +4,7 @@
 Point3D GetNeuronCoords(int x, int y, int z) {
   return { ((float)x) * NEURON_TOTAL_DIST, ((float)y) * NEURON_TOTAL_DIST, ((float)z) * NEURON_TOTAL_DIST };
 }
+
+std::pair<Point3D, Point3D> GetSynapseEndpoints(int x1, int y1, int z1, int x2, int y2, int z2) {
+  
+}
