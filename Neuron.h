29d28bae7297c01aea90c301da4953edd2a04b13
joeyst <joeystenbeck@gmail.com>
2023-12-08 04:27:40 -0800

    Change Neuron._fires and Neuron.NumberOfFires to private.

--- Neuron.h
+++ Neuron.h
@@ -10,14 +10,17 @@ class Neuron {
   _fires | Each bit represents whether the neuron fired during the last 32 ticks. 
   */
   public:
+    float energy;
+
     Neuron(float initialEnergy);
     Neuron();
-    float energy;
-    uint32_t _fires; 
-    int NumberOfFires();
     void SetCurrentTickFire(bool did_fire);
     bool IsFiring();
     float GetTolFactor();
+
+  private:
+    uint32_t _fires; 
+    int NumberOfFires();
 };
 
 #endif 
