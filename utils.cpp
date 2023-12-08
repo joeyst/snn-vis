8f8be44975e0c1f9ee50168ef911a34caa303acd
joeyst <joeystenbeck@gmail.com>
2023-12-07 19:31:20 -0800

    Add GetTolFactor.

--- utils.cpp
+++ utils.cpp
@@ -1,5 +1,6 @@
 
 #include "utils.h"
+#include "cstdlib"
 
 std::pair<Point3D, Point3D> GetConnectionEndpoints(const Point3D& start, const Point3D& end, float segmentPercent) {
     float xOffset = (end.x - start.x) * segmentPercent;
@@ -10,4 +11,12 @@ std::pair<Point3D, Point3D> GetConnectionEndpoints(const Point3D& start, const P
     Point3D newEnd = {end.x - xOffset, end.y - yOffset, end.z - zOffset};
 
     return std::make_pair(newStart, newEnd);
+}
+
+float GetTolFactor(int n_fires) {
+  /* Fast Sigmoid-ish function. */
+  float numer = 2 * (FIRING_RATE_GOAL - (float)n_fires);
+  float denom = 2 + 2 * std::abs((float)n_fires - FIRING_RATE_GOAL);
+  float C     = 1.f;
+  return (numer / denom) + C;
 }
--- utils.h
+++ utils.h
@@ -6,3 +6,7 @@ struct Point3D {
 };
 
 std::pair<Point3D, Point3D> GetConnectionEndpoints(const Point3D& start, const Point3D& end, float segmentPercent); 
+
+const float FIRING_RATE_GOAL = 4.f; 
+float GetTolFactor(int n_fires);
+
