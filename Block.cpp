12b5de286ae603f77cdb51e5aa472dacfcaa2b1a
joeyst <joeystenbeck@gmail.com>
2023-12-08 02:41:56 -0800

    Add .gitignore, BlockId and NeuronMatrix typedefs.

--- Block.cpp
+++ Block.cpp
@@ -3,5 +3,5 @@
 #include "config.h"
 
 Block::Block() {
-  std::vector<std::vector<Neuron>> neurons(LAYERS_PER_BLOCK, std::vector<Neuron>(NEURONS_PER_LAYER));
+  NeuronMatrix neurons(LAYERS_PER_BLOCK, std::vector<Neuron>(NEURONS_PER_LAYER));
 }
--- Block.h
+++ Block.h
@@ -1,14 +1,21 @@
 
 #include "Neuron.h"
 #include <vector>
+#include <utility>
 
 #ifndef _BLOCK_H
 #define _BLOCK_H
 
+typedef int BlockId;
+typedef std::vector<std::vector<Neuron>> NeuronMatrix;
+
 class Block {
   public: 
     Block();
-    std::vector<std::vector<Neuron>> neurons;
+    NeuronMatrix neurons;
+
+  //private:
+  //  std::vector<std::pair<BlockId, > recvs
 };
 
 #endif 
